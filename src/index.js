93bbd67d9aed8bf2381b1d37f4af220448ea70af
Mobiuus-learn <63170559+Mobiuus-learn@users.noreply.github.com>
2022-10-09 16:42:26 -0300

    first

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const fibraIni = document.querySelector('#fib-ini')
+const fibraFim = document.querySelector('#fib-fim')
+const button = document.querySelector('#btn-send')
+const reset = document.querySelector('#btn-reset')
+const fusionResult = document.querySelector('.fusion-result')
+
+let span = document.createElement('span')
+
+const  calcFusion = ()=>{
+    fusionResult.appendChild(span)
+
+    init = Number(fibraIni.value)
+    end = Number(fibraFim.value)
+
+    
+    if(init !=0 && init >= 1){
+        let result = end - init + 1
+        span.innerText = result
+        console.log(result, init, end)
+    }
+    
+    // resetV(span)
+   
+}
+
+const resetV = ()=>{
+    span.innerText =""
+    fibraIni.value = ""
+    fibraFim.value = ""
+}
+button.onclick = calcFusion
+
+reset.onclick = resetV 
