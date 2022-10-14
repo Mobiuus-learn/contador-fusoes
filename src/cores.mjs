79b578514e5670b397360c226b7223114dc24698
Mobiuus-learn <63170559+Mobiuus-learn@users.noreply.github.com>
2022-10-14 20:20:34 -0300

    mudanças e pequena refatoração

---
+++ src/cores.mjs
@@ -0,0 +1,15 @@
+const colorOptions = [
+    {'cor':'verde', id:1},
+    {'cor':'amarelo', id:2},
+    {'cor':'branco', id:3},
+    {'cor':'azul', id:4},
+    {'cor':'vermelho', id:5},
+    {'cor':'violeta', id:6},
+    {'cor':'marrom', id:7},
+    {'cor':'rosa', id:8},
+    {'cor':'preto', id:9},
+    {'cor':'cinza', id:10},
+    {'cor':'laranja', id:11},
+    {'cor':'aqua', id:12}
+]
+export { colorOptions as cores }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,80 @@
+import { cores } from './cores.mjs'
+
 const fibraIni = document.querySelector('#fib-ini')
 const fibraFim = document.querySelector('#fib-fim')
 const button = document.querySelector('#btn-send')
+const alimentador = document.querySelector("#cable-ali")
 const reset = document.querySelector('#btn-reset')
 const fusionResult = document.querySelector('.fusion-result')
-
 let span = document.createElement('span')
 
-const  calcFusion = ()=>{
+
+// opçoes  de cabos comumente usados
+const validInput = [ 12, 24, 36, 48, 72, 144]
+
+
+// grupos
+const calcGroups = (res)=>{
+    let lenghtGroup = res
+    let colors = []
+    colors.lenght = lenghtGroup
+    
+    console.log(colors)
+    for(let i = 0; i <= colors.length; i++){
+        cores.length = colors.length
+        console.log(cores[i].cor)
+    }
+}   
+console.log(cores[4].cor)
+
+const  calcFusion = () =>{
+    let init = parseInt(fibraIni.value)
+    let end = parseInt(fibraFim.value)   
+    let result
     fusionResult.appendChild(span)
 
-    init = Number(fibraIni.value)
-    end = Number(fibraFim.value)
+    if(checkValidate()){
+            calc()
+    }    
 
-    
-    if(init !=0 && init >= 1){
-        let result = end - init + 1
-        span.innerHTML = `<span class="box mt-4">Quantidade total de fusões: ${result}</span>`
-        console.log(result, init, end)
+    function calc() {
+        if (init != 0 && init >= 1) {
+            result = end - init + 1
+            show(result)
+            calcGroups(result)
+
+          
+        }
     }
-    
-    // resetV(span)
+
    
+}
+const show = (res) => {
+    return span.innerHTML = `<span class="box mt-4 has-text-success">Quantidade total de fusões: ${res}</span>`
+}
+
+const checkValidate = () =>{
+           
+    let ali = parseInt(alimentador.value)   
+    const valid = validInput.includes(ali)
+       
+        if(!valid || 0){
+            return alert("Informe uma das opções válidas")
+        }
+        if(!ali){
+            return alert("Por favor, informe o cabo alimentador")
+            
+        }
+    
+        
+        return true
 }
 
 const resetV = ()=>{
     span.innerText =""
     fibraIni.value = ""
     fibraFim.value = ""
+    alimentador.value = ""
 }
 button.onclick = calcFusion
 
